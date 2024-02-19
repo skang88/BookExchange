bbb8aff2ef0505b19d1551f325d5571308eeb16e
Seokgyun, Kang <leadgoon@gmail.com>
2024-02-19 10:42:42 -0700

    Hi skang88, I made change.

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose'); // Mongoose 모듈 추가
 // this is new from windows 10, leadgoon
 // This is from testing btanch
 // This is from skang 88, main branch
+// Hi skang88, I commmint to main branch also. 
 // required for the application to run
 const app = require('./app');
 
